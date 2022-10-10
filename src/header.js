585a8dcff9c83d725e1bf68d8758ae18f41e98d4
Weronika <WerRic96@protonmail.com>
2022-10-10 20:58:06 +0200

    Add text to buttons

--- src/header.js
+++ src/header.js
@@ -14,6 +14,19 @@ function addHeader() {
         const button = document.createElement("button");
         header.appendChild(button);
     }
+
+    const homeButton = document.querySelector(".header button:nth-of-type(1)");
+    homeButton.value = "Home";
+    homeButton.textContent = "Home";
+
+    const menuButton = document.querySelector(".header button:nth-of-type(2)");
+    menuButton.value = "Menu";
+    menuButton.textContent = "Menu";
+
+    const contactButton = document.querySelector(".header button:nth-of-type(3)");
+    contactButton.value = "Contact";
+    contactButton.textContent = "Contact";
 }
+
 
 export {addHeader};
--- src/index.js
+++ src/index.js
@@ -4,8 +4,22 @@ import {addFooter} from "./footer";
 
 
 const tabs = ["home", "menu", "contact"];
-let activeTab = tabs[0];
+// let activeTab = tabs[0];
 
 addHeader();
 mainContent();
+
+function buttonListener() {
+    const buttons = document.querySelectorAll(".header button")
+    .forEach(button => {
+        button.addEventListener("click", setActiveTab);
+    })
+}
+
+function setActiveTab(e) {
+    let activeTab = e.target;
+    alert(activeTab);
+}
+
+buttonListener();
 addFooter();
