f134a5e8ca8b3b1e3e27dc7b82bbf5ba6480da96
Weronika <WerRic96@protonmail.com>
2022-10-12 00:16:06 +0200

    Create table

--- src/menuPage.js
+++ src/menuPage.js
@@ -3,6 +3,17 @@ function createMenuPage() {
     const menuDiv = document.createElement("div");
     menuDiv.classList.toggle(".menu-div");
     main.appendChild(menuDiv);
+    menuDiv.appendChild(createTable());
+}
+
+function getMenuDiv() {
+    return document.querySelector(".menu-div");
+}
+
+function createTable() {
+    const menuDiv = getMenuDiv();
+    const table = document.createElement("table");
+    return table;
 }
 
 export {createMenuPage};
