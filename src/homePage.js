35ed444b690d5600d88fbde8fc7b650f2e238239
Weronika <WerRic96@protonmail.com>
2022-10-11 14:48:27 +0200

    Fisnih order now section

--- src/homePage.js
+++ src/homePage.js
@@ -8,15 +8,31 @@ function createHomePage() {
     const orderDiv = document.createElement("div");
     orderDiv.classList.toggle("order-div");
     homeDiv.appendChild(orderDiv);
+
     const orderNow = document.createElement("h2");
     orderNow.textContent = "Order NOW";
     orderDiv.appendChild(orderNow);
+
     const divLine = document.createElement("div");
     divLine.classList.toggle("div-line");
     orderDiv.appendChild(divLine);
+
     const orderParagraph = document.createElement("p");
     orderParagraph.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quibusdam dolore at repellendus unde quisquam, necessitatibus voluptatum doloribus sapiente, cum temporibus ducimus! Eveniet recusandae placeat qui odit quis id quasi?";
     orderDiv.appendChild(orderParagraph);
+
+    const orderParagraphTwo = document.createElement("p");
+    orderParagraphTwo.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde odit laboriosam ut saepe, repellat explicabo, fugit architecto iste dolores vel earum illum, delectus quidem quaerat soluta. Saepe eveniet nostrum illo?";
+    orderDiv.appendChild(orderParagraphTwo);
+    
+    const divLineTwo = document.createElement("div");
+    divLineTwo.classList.toggle("div-line");
+    orderDiv.appendChild(divLineTwo);
+
+    // order button
+    const orderButton = document.createElement("button");
+    orderButton.textContent = "Order";  
+    orderDiv.appendChild(orderButton);
 }
 
 export{createHomePage};
