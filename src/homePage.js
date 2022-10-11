ce3179ab9003df3348ff69fa727ea855bf46c6d8
Weronika <WerRic96@protonmail.com>
2022-10-11 16:30:54 +0200

    Seperate paragraphs

--- src/homePage.js
+++ src/homePage.js
@@ -46,6 +46,18 @@ function ingredients() {
     const ingredientsHeading = document.createElement("h2");
     ingredientsHeading.textContent = "Healthy Ingredients";
     ingredientsDiv.appendChild(ingredientsHeading);
+
+    const divLine = document.createElement("div");
+    divLine.classList.toggle("div-line");
+    ingredientsDiv.appendChild(divLine);
+
+    const orderParagraph = document.createElement("p");
+    orderParagraph.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quibusdam dolore at repellendus unde quisquam, necessitatibus voluptatum doloribus sapiente, cum temporibus ducimus! Eveniet recusandae placeat qui odit quis id quasi?";
+    ingredientsDiv.appendChild(orderParagraph);
+
+    const orderParagraphTwo = document.createElement("p");
+    orderParagraphTwo.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde odit laboriosam ut saepe, repellat explicabo, fugit architecto iste dolores vel earum illum, delectus quidem quaerat soluta. Saepe eveniet nostrum illo?";
+    ingredientsDiv.appendChild(orderParagraphTwo);
 }
 
 export{createHomePage};
