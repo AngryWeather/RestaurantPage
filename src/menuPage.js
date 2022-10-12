1ce78fba6efd3bd5519848211fca429cce422150
Weronika <WerRic96@protonmail.com>
2022-10-12 20:02:18 +0200

    Add heading rows to table

--- src/menuPage.js
+++ src/menuPage.js
@@ -13,6 +13,18 @@ function getMenuDiv() {
 function createTable() {
     const menuDiv = getMenuDiv();
     const table = document.createElement("table");
+    const row = document.createElement("tr");
+    table.appendChild(row);
+
+    const pizza = document.createElement("th");
+    pizza.textContent = "PIZZA";
+    row.appendChild(pizza);
+
+    const price = document.createElement("th");
+    price.textContent = "PRICE";
+    row.appendChild(price);
+
+    
     return table;
 }
 
